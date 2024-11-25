4c1e8cbe25407d4194978a67aa40fe3754762946
joanalajeel <joantalajeel@gmail.com>
2024-11-25 12:47:37 +0000

    Add files via upload

---
+++ loop_joan.py
@@ -0,0 +1,20 @@
+number=1
+while number <7:
+    print(number)
+    number+=1
+
+symbol=1
+while symbol<6:
+    print('*'* symbol)
+    symbol+=1
+
+for i in range(1,8):
+   print(i)
+
+for symbol in range(1,6):
+   print('*'*symbol)
+
+sum=0
+for number in range(1,11):
+    sum+=number
+print(f"The sum of the numbers between 1-10 is: {sum}")
